4440194d3995157dd4125b0e56d3d6a3990b7fa4
Mohamed00077 <mohamed.diabagate@epitech.eu>
2026-08-31 21:37:46 +0000

    feat: ajouter les commandes uptime et calc

--- commandes.js
+++ commandes.js
@@ -1,3 +1,5 @@
+const {evaluate} = require('mathjs')
+
 //Tableau de commande dynamique
 const commandes = {
     ping: {
@@ -14,7 +16,31 @@ const commandes = {
             }).join('\n')
             await socket.sendMessage(remoteJid, {text: liste})
         }
+    },
+    uptime:{
+        description: "Temps écoulé depuis le demarrage du bot",
+        execute: async(socket, remoteJid, args)=>{
+            const seconde= process.uptime()
+            const heure = Math.floor(seconde/3600)
+            const minuteRestante = Math.floor((seconde % 3600)/60)
+            const secondeRestante = Math.floor(seconde % 60)
+            await socket.sendMessage(remoteJid, {text:`Le bot tourne depuis ${heure}h ${minuteRestante}min ${secondeRestante}s`})
+
+        }
+    },
+    calc:{
+        description: "Mini calculatrice pour effectuer rapidement les opérations de base",
+        execute: async(socket, remoteJid, args)=>{
+           const entrer = args.join(' ')
+           try{
+            const resultat =evaluate(entrer)
+            await socket.sendMessage(remoteJid, {text: `Résultat: ${resultat}`})
+           }catch{
+            await socket.sendMessage(remoteJid, {text: "Expression invalide, réessaie."})
+           }
+        }
     }
+
 }
 
 module.exports= commandes
